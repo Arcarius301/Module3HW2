65096fc06f3fe2ba83b512e59acf7a0dfaf88bcd
Pavel Kaznachei <pavel.kaznachei@gmail.com>
2021-12-08 13:57:29 +0200

    Add contact model.

---
+++ Module3HW2/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3HW2.Models
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+
+        public string PhoneNumber { get; set; }
+    }
+}
